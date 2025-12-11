6d2ecf47ed1611fea8ff7fe27c01f878f9817499
Francisco Avendaño <favendan@espol.edu.ec>
2025-12-11 03:13:09 +0000

    mejoras para deploy en desarrollo (espero)

---
+++ src/js/dashboardP.js
@@ -0,0 +1,9 @@
+import '../style.css'
+
+const btnToindex = document.getElementById("btnToPrincipal");
+
+btnToindex.addEventListener("click", ()=>{
+    window.location.href="index.html";
+});
+
+
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,15 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
-  base: "/KnowNet/"
+  base: "/KnowNet/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        dashboard: resolve(__dirname, 'dashboardP.html'),
+        // mas páginas HTML aquí
+      }
+    }
+  }
 })
